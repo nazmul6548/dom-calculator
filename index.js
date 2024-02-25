1a6a625163d5b3a1c5693e15306229266c996cd5
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2024-02-25 17:07:24 +0600

    task

---
+++ index.js
@@ -0,0 +1,43 @@
+// const f=document.querySelectorAll(".n");
+// const result =document.querySelector('result');
+// for (let i = 0; i < f.length; i++) {
+//     f[i].addEventListener("click",() => {
+// const t =f[i].textContent;
+// const p =document.getElementById('display');
+// p.innerText = t;
+// console.log(p.value);
+//     });
+    
+// }
+
+// document.querySelectorAll(".a").forEach(element => {
+//     element.addEventListener("click", () => {
+//         console.log(element);
+//     });
+// });
+
+
+// const u =document.getElementById("btns");
+//     // const name = u.addEventListener('click',() => {
+//     // const t = name.parentNode;
+//     // console.log(t);
+//     // })
+
+// console.log(u.parentNode.parentNode.childNodes[1].innerText);
+
+
+// function clickme() {
+//     // document.write("amk click kora hoyce");
+//     document.getElementById('btn').innerText =Date()
+// }
+
+const person ={
+    name:"nazmul",
+    adress:{
+        thana:"mirsarai",
+        zilla :"chittagong"
+    }
+}
+const  {adress:{zilla:upozilla}} =person;
+// const {zilla} = adress;
+console.log(upozilla);
